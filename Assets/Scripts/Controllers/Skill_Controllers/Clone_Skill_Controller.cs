dfc41dac42cc4142d4d2603c3a914c458ee71d8d
yingjingliang <yingjingliang@1898476635254215.onaliyun.com>
2025-06-04 20:36:32 +0800

    first commit

---
+++ Assets/Scripts/Controllers/Skill_Controllers/Clone_Skill_Controller.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using UnityEngine;
+
+public class Clone_Skill_Controller : MonoBehaviour
+{
+    private Player player;
+    private SpriteRenderer sr;
+    private Animator anim;
+    [SerializeField] private float colorLoosingSpeed;
+
+    private float cloneTimer;
+    private float attackMultiplier;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = .8f;
+    private int facingDir = 1;
+
+
+    private bool canDuplicateClone;
+    private float chanceToDuplicate;
+
+    [Space]
+    [SerializeField] private LayerMask whatIsEnemy;
+    [SerializeField] private float closestEnemyCheckRadius = 25;
+    [SerializeField] private Transform closestEnemy;
+
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(FaceClosestTarget());
+    }
+
+    private void Update()
+    {
+        cloneTimer -= Time.deltaTime;
+
+        if (cloneTimer < 0)
+        {
+            sr.color = new Color(1, 1, 1, sr.color.a - (Time.deltaTime * colorLoosingSpeed));
+
+            if (sr.color.a <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack, Vector3 _offset, bool _canDuplicate, float _chanceToDuplicate, Player _player, float _attackMultiplier)
+    {
+        if (_canAttack)
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+
+        attackMultiplier = _attackMultiplier;
+        player = _player;
+        transform.position = _newTransform.position + _offset;
+        cloneTimer = _cloneDuration;
+
+        canDuplicateClone = _canDuplicate;
+        chanceToDuplicate = _chanceToDuplicate;
+    }
+
+
+    private void AnimationTrigger()
+    {
+        cloneTimer = -.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                //player.stats.DoDamage(hit.GetComponent<CharacterStats>()); // make a new function for clone damage to regulate damage;
+
+                hit.GetComponent<Entity>().SetupKnockbackDir(transform);
+
+                PlayerStats playerStats = player.GetComponent<PlayerStats>();
+                EnemyStats enemyStats = hit.GetComponent<EnemyStats>();
+
+                playerStats.CloneDoDamage(enemyStats, attackMultiplier);
+
+                if (player.skill.clone.canApplyOnHitEffect)
+                {
+                    ItemData_Equipment weaponData = Inventory.instance.GetEquipment(EquipmentType.Weapon);
+
+                    if (weaponData != null)
+                        weaponData.Effect(hit.transform);
+                }
+
+                if (canDuplicateClone)
+                {
+                    if (Random.Range(0, 100) < chanceToDuplicate)
+                    {
+                        SkillManager.instance.clone.CreateClone(hit.transform, new Vector3(.5f * facingDir, 0));
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator FaceClosestTarget()
+    {
+        yield return null;
+
+        FindClosestEnemy();
+
+        if (closestEnemy != null)
+        {
+            if (transform.position.x > closestEnemy.position.x)
+            {
+                facingDir = -1;
+                transform.Rotate(0, 180, 0);
+            }
+        }
+    }
+
+    private void FindClosestEnemy()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, closestEnemyCheckRadius, whatIsEnemy);
+
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var hit in colliders)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = hit.transform;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, closestEnemyCheckRadius);
+    }
+}
---
+++ Assets/Scripts/Enemy/Skeleton/SkeletonBattleState.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class SkeletonBattleState : EnemyState
+{
+    private Transform player;
+    private Enemy_Skeleton enemy;
+    private int moveDir;
+
+    private bool flippedOnce;
+    public SkeletonBattleState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton _enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        player = PlayerManager.instance.player.transform;
+
+        if (player.GetComponent<PlayerStats>().isDead)
+            stateMachine.ChangeState(enemy.moveState);
+
+        stateTimer = enemy.battleTime;
+        flippedOnce = false;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        enemy.anim.SetFloat("xVelocity", enemy.rb.velocity.x);
+
+        if (enemy.IsPlayerDetected())
+        {
+            stateTimer = enemy.battleTime;
+
+            if (enemy.IsPlayerDetected().distance < enemy.attackDistance)
+            {
+                if (CanAttack())
+                    stateMachine.ChangeState(enemy.attackState);
+            }
+        }
+        else
+        {
+            if (flippedOnce == false)
+            {
+                flippedOnce = true;
+                enemy.Flip();
+            }
+
+            if (stateTimer < 0 || Vector2.Distance(player.transform.position, enemy.transform.position) > 7)
+                stateMachine.ChangeState(enemy.idleState);
+        }
+
+        float distanceToPlayerX = Mathf.Abs(player.position.x - enemy.transform.position.x);
+
+        if (distanceToPlayerX < .8f)
+            return;
+
+        if (player.position.x > enemy.transform.position.x)
+            moveDir = 1;
+        else if (player.position.x < enemy.transform.position.x)
+            moveDir = -1;
+
+        
+        enemy.SetVelocity(enemy.moveSpeed * moveDir, rb.velocity.y);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    private bool CanAttack()
+    {
+        if (Time.time >= enemy.lastTimeAttacked + enemy.attackCooldown)
+        {
+            enemy.attackCooldown = Random.Range(enemy.minAttackCooldown, enemy.maxAttackCooldown);
+            enemy.lastTimeAttacked = Time.time;
+            return true;
+        }
+
+        return false;
+    }
+}
